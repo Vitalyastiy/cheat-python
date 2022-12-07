e90412b4cc3ebe82bf3e3d037d9744187baadf79
Vitalyastiy <118436864+Vitalyastiy@users.noreply.github.com>
2022-12-07 16:12:10 +0300

    Update .python_union csv to 1.py

--- .python_union csv to 1.py
+++ .python_union csv to 1.py
@@ -12,3 +12,19 @@ combined_csv = pd.concat([pd.read_csv(f, sep=";") for f in all_filenames ])
 combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
 #%%
 
+
+
+''' Для экселя'''
+# filenames
+excel_names = ["xlsx1.xlsx", "xlsx2.xlsx", "xlsx3.xlsx"]
+# read them in
+excels = [pd.ExcelFile(name) for name in excel_names]
+# turn them into dataframes
+frames = [x.parse(x.sheet_names[0], header=None,index_col=None) for x in excels]
+# delete the first row for all frames except the first
+# i.e. remove the header row -- assumes it's the first
+frames[1:] = [df[1:] for df in frames[1:]]
+# concatenate them..
+combined = pd.concat(frames)
+# write it out
+combined.to_excel("c.xlsx", header=False, index=False)
